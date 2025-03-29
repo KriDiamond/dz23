c4979cdda7ce44d04b685a9b1f708ab4bc1e025f
Heather Diamond <baishenkokv@gmail.com>
2025-03-29 17:32:20 +0300

    свернула в одну строку

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 def sum_even ():
-    sum = 0
-    i = 0
-    while i < 101:
-        if i%2 == 0:
-            sum += i
-        i += 1
-    return sum
+    return sum([x for x in range(101) if x % 2 == 0])
 
 
 def squares ():
-    return [x * x for x in [1, 3, 5, 7, 9]]
+    return [x * x for x in range(10) if x % 2 == 1]
 
 
-def is_positive(num):
-    return num <= 0
-
+def positive_counts():
+    user_num = 1
+    count_of_positive = -1
+    while user_num > -1:
+        count_of_positive += 1
+        user_num = int(input("Введите положительное число: "))
+    print(f"Введено положительных чисел: {count_of_positive}")
 
 print(f"Сумма четных чисел в интервале от 1 до 100: {sum_even()}")
 print(f"Квадраты нечетных чисел, поcтроенные методом генератора списков: {squares()}")
-user_num = 1
-count_of_positive = -1
-while not is_positive(user_num):
-    count_of_positive +=1
-    user_num = int(input("Введите положительное число: "))
-print(f"Введено положительных чисел: {count_of_positive}")
+positive_counts()
+
+
 
